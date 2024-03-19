c995455149bb79f0f14f42b9bdb8e976222b1d2d
Max Mynter <maxmynter@me.com>
2024-03-19 13:20:26 +0100

    Change Message for pipeline

--- part3/ex3.1/express-app/index.js
+++ part3/ex3.1/express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World! This change is reflected when i run the pipeline</h1>')
+  res.send('<h1>Hello World! This change is reflected when I run the pipeline</h1>')
 })
 
 const PORT = 8080
